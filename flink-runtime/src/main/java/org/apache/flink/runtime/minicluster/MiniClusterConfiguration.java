b815f2d5fdcaf9a710731ffde654417481346c8d
Robert Metzger <89049+rmetzger@users.noreply.github.com>
2023-04-11 16:55:09 +0200

    DE-4022: Make MiniCluster configurable (#52)
    * DE-4022: Make MiniCluster configurable
    
    * [FLINK-27640][Connector/Hive][SQL Client] Exclude Pentaho dependency from Hive to avoid build problems with newer Maven versions. Newer Maven versions block http repositories such as conjars. Conjars is where Pentaho artifacts are stored (#22299)
    
    Co-authored-by: Martijn Visser <martijnvisser@apache.org>
    
    ---------
    
    Co-authored-by: Sergey Nuyanzin <snuyanzin@gmail.com>
    Co-authored-by: Martijn Visser <martijnvisser@apache.org>

--- flink-runtime/src/main/java/org/apache/flink/runtime/minicluster/MiniClusterConfiguration.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/minicluster/MiniClusterConfiguration.java
@@ -20,12 +20,15 @@ package org.apache.flink.runtime.minicluster;
 
 import org.apache.flink.configuration.AkkaOptions;
 import org.apache.flink.configuration.ClusterOptions;
+import org.apache.flink.configuration.ConfigOption;
+import org.apache.flink.configuration.ConfigOptions;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.JobManagerOptions;
 import org.apache.flink.configuration.NettyShuffleEnvironmentOptions;
 import org.apache.flink.configuration.RestOptions;
 import org.apache.flink.configuration.TaskManagerOptions;
 import org.apache.flink.configuration.UnmodifiableConfiguration;
+import org.apache.flink.configuration.description.Description;
 import org.apache.flink.core.plugin.PluginManager;
 import org.apache.flink.runtime.taskexecutor.TaskExecutorResourceUtils;
 import org.apache.flink.util.Preconditions;
@@ -38,6 +41,32 @@ import static org.apache.flink.runtime.minicluster.RpcServiceSharing.SHARED;
 
 /** Configuration object for the {@link MiniCluster}. */
 public class MiniClusterConfiguration {
+
+    public static final ConfigOption<Duration> GATEWAY_RETRIEVER_INITIAL_BACKOFF =
+            ConfigOptions.key("minicluster.initial-backoff")
+                    .durationType()
+                    .defaultValue(Duration.ofMillis(10))
+                    .withDescription(
+                            Description.builder()
+                                    .text("The initial duration between retrieval attempts.")
+                                    .build());
+    public static final ConfigOption<Duration> GATEWAY_RETRIEVER_MAX_BACKOFF =
+            ConfigOptions.key("minicluster.max-backoff")
+                    .durationType()
+                    .defaultValue(Duration.ofMillis(20))
+                    .withDescription(
+                            Description.builder()
+                                    .text(
+                                            "The highest possible duration between retrieval retries.")
+                                    .build());
+    public static final ConfigOption<Integer> GATEWAY_RETRIEVER_MAX_ATTEMPTS =
+            ConfigOptions.key("minicluster.max-attempts")
+                    .intType()
+                    .defaultValue(21)
+                    .withDescription(
+                            Description.builder()
+                                    .text("The number of mini cluster gateway retrieval attempts.")
+                                    .build());
 
     static final int DEFAULT_IO_POOL_SIZE = 4;
 
